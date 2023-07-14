b26f1e77a42f05694be212a0f7f5aff27cec75ce
BuRZone <127656841+BuRZone@users.noreply.github.com>
2023-07-14 21:40:59 +0300

    add builder.hasdata, add public int BasketId to User ent.

--- OnlineMarket.DAL/ApplicationDbContext.cs
+++ OnlineMarket.DAL/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OnlineMarket.DAL.Entity;
+using System.Net.Http.Headers;
 
 namespace OnlineMarket.DAL
 {
@@ -24,6 +25,12 @@ namespace OnlineMarket.DAL
                 builder.ToTable(nameof(Category)).HasKey(p => p.Id);
                 builder.HasMany(p => p.Product);
                 builder.HasMany(p => p.Seller);
+                builder.HasData(new Category[]
+                    {
+                        new Category(){ Id=1, CategoryName = "одежда"},
+                        new Category(){ Id=2, CategoryName = "автомобили"},
+                        new Category(){ Id=3, CategoryName = "товары для животных"}
+                    });
             });
             modelBuilder.Entity<Product>(builder =>
             {
@@ -32,17 +39,27 @@ namespace OnlineMarket.DAL
                 builder.HasMany(p => p.Seller);
             });
             modelBuilder.Entity<Seller>().ToTable(nameof(Seller));
+
+
             modelBuilder.Entity<User>(builder =>
             {
                 builder.ToTable(nameof(User)).HasKey(p => p.Id);
                 builder.HasOne(p => p.Basket).WithOne(p => p.User).
                 HasPrincipalKey<User>(p => p.Id).OnDelete(DeleteBehavior.Cascade);
+                builder.HasData(new User()
+                {
+                    Id = 1,
+                    UserName = "Mike",
+                    Role = Enum.Role.Admin,
+                    Password = "123456"  
+                });
             });
             modelBuilder.Entity<Basket>(builder =>
             {
                 builder.ToTable(nameof(Basket)).HasKey(p => p.Id);
                 builder.HasOne(p => p.User);
                 builder.HasMany(p => p.Product);
+                builder.HasData(new Basket() { Id = 1, UserId = 1 });
             });
             modelBuilder.Entity<Order>(builder =>
             {
