6f61b9b7218811d34d50f5ab608b47df5f948015
BuRZone <127656841+BuRZone@users.noreply.github.com>
2023-07-26 03:04:08 +0300

    add sidebar meny with overlay

---
+++ OnlineMarket/Components/CategoryViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineMarket.BLL.Service.Interfaces;
+using OnlineMarket.BLL.ViewModels.Category;
+
+namespace OnlineMarket.Components
+{
+    [ViewComponent]
+    public class CategoryViewComponent : ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryViewComponent(ICategoryService catetogyService)
+        {
+            _categoryService = catetogyService;
+        }
+
+        public List<CategoryVM> categoryVMList = new List<CategoryVM>();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categoryList = await _categoryService.GetAsync().ToListAsync();
+
+            foreach (var categroy in categoryList)
+            {
+                CategoryVM categoryVM = new CategoryVM();
+                categoryVM.CategoryName = categroy.CategoryName;
+                categoryVM.Id = categroy.Id;
+                categoryVMList.Add(categoryVM);
+            }
+
+            return View("CategoryViewComponent", categoryVMList);
+        }
+
+    }
+}
--- OnlineMarket/Controllers/SubCategoryController.cs
+++ OnlineMarket/Controllers/SubCategoryController.cs
@@ -5,8 +5,6 @@ using OnlineMarket.BLL.ViewModels.Category;
 
 namespace OnlineMarket.Controllers
 {
-    
-    
     public class SubCategoryController : Controller
     {
         private readonly ISubCategoryService _subCategoryService;
@@ -15,22 +13,28 @@ namespace OnlineMarket.Controllers
         {
             _subCategoryService = subCategoryService;
             _categoryService = categoryService;
-
         }
 
-        public IActionResult GetSubCategory()
+        public IActionResult GetSubCategory(int? id)
         {
+            if (id == null)
+            {
+                return NotFound();
+            }
             List<SubCategoryVM> subCategoryVMList = new List<SubCategoryVM>();
             return View(subCategoryVMList);
         }
 
-
-        [HttpGet]
-        public async Task<IActionResult> GetSubCategory(int? id)
+        [HttpPost]
+        public async Task<IActionResult> GetSubCategory(int id)
         {
+            if (id == null)
+            {
+                return NotFound();
+            }
             var subCatVMList = new List<SubCategoryVM>();
 
-            var categoryQ = await _categoryService.GetAsync().FirstOrDefaultAsync(x => x.Id==id);
+            var categoryQ = await _categoryService.GetAsync().Include(x => x.SubCategory).FirstOrDefaultAsync(x => x.Id == id);
 
             foreach (var subCat in categoryQ.SubCategory)
             {
